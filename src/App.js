9ff5af8b83f6059b7ab08b06c74feed4cf8ed0cc
Djagz <jagwanidipesh85@gmail.com>
2019-05-30 15:46:48 -0700

    Initial TodoList
    -Main component is App
    -It shows the TodoList
    -Both input text box and button have the event handlers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {connect} from 'react-redux'
 
-function App() {
+class App extends React.Component {
+    //let deleteBtn = 
+    render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <input 
+      type="text" 
+      placeholder="Enter Items"
+      onChange={this.props.onHandleChange} />
+      <br />
+      <button onClick={this.props.onHandleClick}>Add</button>
+
+      <br />
+
+      <ol>
+        {this.props.list.map((listItem, index)=>{
+          return(
+            <div>
+              <li>{listItem} <button onClick={this.props.onHandleDelete.bind(this, index)}>Delete</button></li>
+            </div>
+            )
+        })}
+      </ol>
+        
       </header>
     </div>
   );
 }
+}
 
-export default App;
+const mapStateToProps = (state) => {
+  return{
+    str: state.str,
+    list: state.list
+  }
+
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    onHandleChange: (e) => dispatch({type: 'inputChange', val: e.target.value}),
+    onHandleClick: () => dispatch({type: 'addItem'}),
+    onHandleDelete: (id) => dispatch({type: 'deleteItem', val: id})
+  }
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
